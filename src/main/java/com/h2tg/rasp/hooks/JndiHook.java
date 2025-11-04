e1d829016891c989b22819e75f4ad5ca95e63f3e
H4cking2theGate <h4cking2thegate@gmail.com>
2025-11-04 21:15:06 +0800

    remove RMI codebase blocking functionality from JndiHook

--- src/main/java/com/h2tg/rasp/hooks/JndiHook.java
+++ src/main/java/com/h2tg/rasp/hooks/JndiHook.java
@@ -55,31 +55,5 @@ public class JndiHook {
         }
     }
 
-    /**
-     * Hook for sun.rmi.server.LoaderHandler.lookupLoader
-     * Blocks any RMI codebase (remote class loading)
-     */
-    @HookHandler(
-            hookClass = "sun.rmi.server.LoaderHandler",
-            hookMethod = "lookupLoader"
-    )
-    public static class RMILookupLoaderAdvice {
 
-        @Advice.OnMethodEnter
-        static void onEnter(@Advice.Argument(0) Object urls) {
-            if (urls == null) {
-                return;
-            }
-
-            if (urls instanceof java.net.URL[]) {
-                java.net.URL[] urlArray = (java.net.URL[]) urls;
-                if (urlArray.length == 0) {
-                    return;
-                }
-
-                System.err.println("[MicroRASP] [BLOCKED] RMI codebase: " + java.util.Arrays.toString(urlArray));
-                throw new SecurityException("MicroRASP blocked RMI codebase: " + java.util.Arrays.toString(urlArray));
-            }
-        }
-    }
 }
